9e6e67314bd02385f1422a5e17a2e723a7b7d7c7
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2024-04-10 17:34:09 -0700

    Edge Waterfalls and Other Cleanup
    This has some weird interactions with background, but that should make more sense when it's a sky box and not a cave wall.

--- scripts/waterfall.lua
+++ scripts/waterfall.lua
@@ -2,7 +2,7 @@ local mod = mod_loader.mods[modApi.currentMod]
 local customAnim = mod.libs.customAnim
 
 --waterfall name and tileset
-local TILESET = "Nautilus_tileset_id"
+local TILESET = "Skylance_tileset_id"
 local WATERFALL = TILESET .. "_Waterfall"
 
 ----------------
@@ -36,6 +36,22 @@ ANIMS['0'..WATERFALL] = ANIMS[WATERFALL]:new{
 
 local order = {3,0,1,2} -- needed to align waterfall position and sprite display order
 
+ANIMS['3'..WATERFALL.."_edge"] = ANIMS[WATERFALL]:new{
+    Image = "combat/tiles_"..TILESET.."/waterfall_U.png", PosX = -28+28, PosY = 25-21,Layer = LAYER_FLOOR
+}
+
+ANIMS['2'..WATERFALL.."_edge"] = ANIMS[WATERFALL]:new{
+    Image = "combat/tiles_"..TILESET.."/waterfall_L.png", PosX = -10-28, PosY = 25-21,Layer = LAYER_FLOOR
+}
+
+ANIMS['1'..WATERFALL.."_edge"] = ANIMS[WATERFALL]:new{
+    Image = "combat/tiles_"..TILESET.."/waterfall_D.png", PosX = -11-28, PosY = 7+21,Layer = LAYER_FRONT
+}
+
+ANIMS['0'..WATERFALL.."_edge"] = ANIMS[WATERFALL]:new{
+    Image = "combat/tiles_"..TILESET.."/waterfall_R.png", PosX = -27+28, PosY = 7+21,Layer = LAYER_FRONT
+}
+
 ---------------
 -- FUNCTIONS --
 ---------------
@@ -46,26 +62,49 @@ local function exceptionList(point)
 	if
 		mission and mission.Incinerator and mission.Incinerator == point
 	then
-		return false
-	else
 		return true
+	else
+		return false
 	end
 end
 
+local function isWaterTile(point)
+	return Board:GetTerrain(point) == TERRAIN_WATER and
+		Board:GetTerrain(point) ~= TERRAIN_ICE and
+		not Board:IsTerrain(point,TERRAIN_LAVA) and
+		not Board:IsAcid(point)
+end
+
+
 local function waterfallTile(point)
+	if not Board:IsValid(point) then
+		return
+	end
+
 	for i,j in ipairs(order) do -- re-do waterfalls on tile
 		customAnim:rem(point,(i-1)..WATERFALL)
-		if Board:GetTerrain(point) == TERRAIN_HOLE then
+		customAnim:rem(point,(i-1)..WATERFALL.."_edge") --Also remove edge
+		if Board:GetTerrain(point) == TERRAIN_HOLE and not exceptionList(point) then
 			local curr = point + DIR_VECTORS[j]
-			if Board:IsValid(curr) then
-				if Board:GetTerrain(curr) == TERRAIN_WATER and Board:GetTerrain(curr) ~= TERRAIN_ICE then
-					if not Board:IsTerrain(curr,TERRAIN_LAVA) and not Board:IsAcid(curr) then
-						if exceptionList(point) then
-							customAnim:add(point,(i-1)..WATERFALL)
-						end
-					end
-				end
+			if Board:IsValid(curr) and isWaterTile(curr) then
+				customAnim:add(point,(i-1)..WATERFALL)
 			end
+		end
+	end
+
+	--Edge Waterfalls
+	if isWaterTile(point) then
+		if (point.x == 0) then
+			customAnim:add(point,"2"..WATERFALL.."_edge")
+		end
+		if (point.x == 7) then
+			customAnim:add(point,"0"..WATERFALL.."_edge")
+		end
+		if (point.y == 0) then
+			customAnim:add(point,"3"..WATERFALL.."_edge")
+		end
+		if (point.y == 7) then
+			customAnim:add(point,"1"..WATERFALL.."_edge")
 		end
 	end
 end
@@ -73,21 +112,7 @@ end
 local function waterfallAdjacent(point)
 	for d = DIR_START, DIR_END do -- re-do waterfalls on adjacent
 		local loc = point + DIR_VECTORS[d]
-		for i,j in ipairs(order) do
-			customAnim:rem(loc,(i-1)..WATERFALL)
-			if Board:IsValid(loc) and Board:GetTerrain(loc) == TERRAIN_HOLE then
-				local curr = loc + DIR_VECTORS[j]
-				if Board:IsValid(curr) then
-					if Board:GetTerrain(curr) == TERRAIN_WATER and Board:GetTerrain(curr) ~= TERRAIN_ICE then
-						if not Board:IsTerrain(curr,TERRAIN_LAVA) and not Board:IsAcid(curr) then
-							if exceptionList(loc) then
-								customAnim:add(loc,(i-1)..WATERFALL)
-							end
-						end
-					end
-				end
-			end
-		end
+		waterfallTile(loc)
 	end
 end
 
